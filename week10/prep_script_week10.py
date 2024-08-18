e27b10c79703f7d22599569c5914ff19cbbafda7
jnaiman <jnaiman@illinois.edu>
2024-08-18 14:11:11 -0500

    w11

--- week10/prep_script_week10.py
+++ week10/prep_script_week10.py
@@ -1,11 +1,12 @@
 ###############################################
-#  1. Starting off building streamlit apps 
+#  1. Intro to Streamlit -- Text & Layouts
 ###############################################
 # Start off in PL!  Open up the "ungraded VSCode" space and make the "inclass" notebook
 
 # For Streamlit apps, we'll need to write our apps in .py files, not notebook files like we've been doing so far.
 # For this, we'll be using VSCode to do this.
-# (Note: there is technically a jupyterlab wrapper for Streamlit, but its just been developed so we won't be using it for this class.)
+# (Note: there is technically a jupyterlab wrapper for Streamlit, but its just been 
+#    developed so we won't be using it for this class.)
 
 # Let's start with running a very simple streamlit app:
 import streamlit as st
@@ -22,20 +23,47 @@ st.title('This is my fancy app!')
 # 4. Open a Simple Browswer with View --> Command Palette --> Simple Browser: Show
 # 5. use the URL from prior steps as intput into this simple browser
 
-#**HERE** -- more about streamlit like here:
-#* https://docs.streamlit.io/get-started/fundamentals/main-concepts (add to reading in syllabus & day page)
-# do different text types!
-# Altair plots
+### 1.1 Text in Streamlit ###
+# Let's look at a few more ways to include text in our Streamlit apps.
+# We usually start our app with a title (like what we had above).
+# We can also have headers and subheaders.
+# See: https://docs.streamlit.io/develop/api-reference/text 
 
-# day 2/3
-# touch on widgets! -- do quick example of plot + widget, say we'll focus on Altair
-# multi-page apps? ==> maybe day 2? ==> does this work with HF apps??
-# matplotlib plots
-#* https://docs.streamlit.io/develop/tutorials/databases <- touch on but say we'll be just doing csv files
+st.header('This is a "header"')
+st.subheader('This is a "subheader"')
+
+# On the docs there are several other ways to use text (like LaTeX and Markdown),
+#  but we'll usually just be using the plain text:
+st.text("This is plain text.")
+
+# You can also use "magic" commands like:
+'This is also plain text!!'
+
+# ... but we will be using the "write" command typically for consistency:
+st.write('This is also some text.')
+# You shoudl do whatever makes sense to you!
+
+### 1.2 Layout elements ###
+# There are several different ways we can layout our text/charts.
+# See: https://docs.streamlit.io/develop/api-reference/layout 
+
+col1, col2 = st.columns(2)
+col1.write('This is me adding in some text to column 1')
+col2.write('This is me adding in some text to column 2')
+
+# Note that in theory we can have multiple columns, but in practice 
+#  the columns will "wrap" after a certain number by default.
+
+# There is a lot of fun stuff here to play with in layouts! 
+# For our purposes, we'll start off with some of the simple defaults.
+
+
 
 ###############################################
 #  2. Vega-lite in Streamlit
 ###############################################
+
+st.header('Vega-lite in Streamlit')
 
 # While we'll be focusing on making Altair-based plots in Streamlit, if vega-lite is your jam,
 # you can absolutely make vega-lite based plots with "st.vega_lite_chart": 
@@ -93,11 +121,32 @@ st.vega_lite_chart(spec=spec)
 #  4. Altair in Streamlit
 ###############################################
 
-# We will spend most of our focus will be on 
+st.header('Altair in Streamlit')
+
+# We will spend most of our focus will be on using Altair to 
+#  make charts, but we'll circle back to look at more complex 
+#  things in the next few weeks as well.
 
 import altair as alt
 
+# Let's recall a plot that we made with Altair in Jupyterlab:
+#    Make sure we copy the URL as well!
+mobility_url = 'https://raw.githubusercontent.com/UIUC-iSchool-DataViz/is445_data/main/mobility.csv'
 
+
+scatters = alt.Chart(mobility_url).mark_point().encode(
+    x='Mobility:Q', # "Q for quantiative"
+    #y='Population:Q',
+    y=alt.Y('Population:Q', scale=alt.Scale(type='log')),
+    color=alt.Color('Income:Q', scale=alt.Scale(scheme='sinebow'),bin=alt.Bin(maxbins=5))
+)
+scatters # note we can use the "magic" display here
+
+st.write('We can also use some of the layout elements to enhance our plot instead of just using the "magic" function:')
+
+col1,col2 = st.columns(2)
+col1.altair_chart(scatters, theme="streamlit", use_container_width=True)
+col2.write("Here is some text that I can write on the side of my plot.")
 
 
 # ****HERE****
---
+++ week11/prep_script_week11.py
@@ -0,0 +1,11 @@
+
+
+
+# day 2/3 -- "grab bag" of other things
+# touch on widgets! -- do quick example of plot + widget, say we'll focus on Altair
+# multi-page apps? ==> maybe day 2? ==> does this work with HF apps??
+# matplotlib plots
+# other plotting tools in ST (like the defaults) + widgets
+#* https://docs.streamlit.io/develop/tutorials/databases <- touch on but say we'll be just doing csv files
+# embedding streamlit spaces on other webpages? wait until Jekyll? https://huggingface.co/docs/hub/en/spaces-sdks-streamlit#embed-streamlit-spaces-on-other-webpages
+# how to search/duplicate other spaces on HF (make sure you cite this!)
