050366b086ad73b28ef37d8538737c96aa3cb8ac
jnaiman <jnaiman@illinois.edu>
2024-10-28 13:22:45 -0500

    Auto-commit: Mon Oct 28 13:22:45 CDT 2024

--- week10/inClass_script_week10.py
+++ week10/inClass_script_week10.py
@@ -1,2 +1,9 @@
-# Start off in PL!  Open up the "ungraded VSCode" space and make the "inclass" notebook
+# we will often times use an app.py instead of a workbook so here is just a placeholder for that kind of file as well!
+import streamlit as st
 
+st.title('This is my fancy app!')
+
+st.header("This is a header")
+st.subheader('This is a subheader')
+
+st.text('This is some text.')
