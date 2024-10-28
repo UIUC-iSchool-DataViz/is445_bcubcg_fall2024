590491261b21804730cd81186ad681a9fbad604f
jnaiman <jnaiman@illinois.edu>
2024-10-28 13:27:28 -0500

    Auto-commit: Mon Oct 28 13:27:28 CDT 2024

--- week10/inClass_script_week10.py
+++ week10/inClass_script_week10.py
@@ -19,3 +19,10 @@ st.subheader('Images')
 st.image('https://i.redd.it/on-a-scale-of-1-10-how-derpy-is-she-v0-z8gtdwu5n5zb1.jpg?width=3024&format=pjpg&auto=webp&s=345e7e1d5b45f20c733e497a9f746f4cbd3a61da',
          width=200,
          caption='A thinly veiled excuse for a cute corg!')
+
+
+import numpy as np
+
+img_data = np.random.random((200,200))
+st.image(img_data, 
+         caption='Random numpy data')
