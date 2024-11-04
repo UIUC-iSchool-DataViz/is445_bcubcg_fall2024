a9928faa7d26d295f6cd9087526f074f277105c2
jnaiman <jnaiman@illinois.edu>
2024-11-04 13:20:47 -0600

    Auto-commit: Mon Nov  4 13:20:47 CST 2024

--- week11/inClass/inClass_script_week10.py
+++ week11/inClass/inClass_script_week10.py
@@ -96,7 +96,7 @@ if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
          """)
 
-sentiment_mapping = ["one", "two", "three", "four", "five"]
-selected = st.feedback("stars")
-if selected is not None:
-    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+# sentiment_mapping = ["one", "two", "three", "four", "five"]
+# selected = st.feedback("stars")
+# if selected is not None:
+#     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
